79289e699fc61ff694d9c5404afe3055d3682c80
ouajih_zakia <zakiaouajih12@gmail.com>
2026-03-22 21:28:27 +0100

    linking search by button and search by floor

--- Assets/Scripts/UiManagerExplore.cs
+++ Assets/Scripts/UiManagerExplore.cs
@@ -6,16 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class UIManagerExplore : MonoBehaviour
 {
+    public GameObject categoryButtons;  // parent of category buttons
+    public GameObject scrollView;       // the scroll view
     public GameObject searchExplore;
     // Start is called before the first frame update
     void Start()
     {
         searchExplore.SetActive(true);
+
+        categoryButtons.SetActive(true);
+        scrollView.SetActive(false);
+        
     }
 
 
     public void GoToWelcome()
     {
         SceneManager.LoadScene("WelcomeScene"); // use the exact scene name
+    }
+
+    public void ShowScrollView()
+    {
+        categoryButtons.SetActive(false);
+        scrollView.SetActive(true);
+        
     }
 }
